60be7d08fb906745cbc7c898515b9803b9da0700
kir4che <mollydcxxiii@gmail.com>
2024-01-23 03:57:18 +0800

    Update

--- client/src/pages/admin/dashboard/index.tsx
+++ client/src/pages/admin/dashboard/index.tsx
@@ -59,10 +59,10 @@ const Dashboard = () => {
   const handleFormOpen = (item, data) => {
     if (item === 'products') {
       setProductForm(data)
-      setTimeout(() => setIsAddOrEditProductForm(data.title === '' ? 'add' : 'edit'), 5000)
+      setIsAddOrEditProductForm(data.title === '' ? 'add' : 'edit')
     } else {
       setPostForm(data)
-      setTimeout(() => setIsAddOrEditPostForm(data.title === '' ? 'add' : 'edit'), 5000)
+      setIsAddOrEditPostForm(data.title === '' ? 'add' : 'edit')
     }
   }
 
@@ -192,7 +192,10 @@ const Dashboard = () => {
                   <li className='flex items-center justify-between py-3 text-xs border-b border-gray-300 border-dashed cursor-pointer' key={product._id}>
                     <p className='hover:opacity-80'>{product.title}</p>
                     <div className='space-x-4'>
-                      <button className='hover:opacity-60' onClick={() => handleFormOpen('products', product)}>
+                      <button className='hover:opacity-60' onClick={() => {
+                        setProductForm(product)
+                        setIsAddOrEditProductForm('edit')
+                      }}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 50 50">
                           <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z"></path>
                         </svg>
@@ -207,23 +210,27 @@ const Dashboard = () => {
                   </li>
                 ))}
               </ul>
-              <button className='block mt-3 ml-auto text-sm w-fit text-secondary hover:text-primary hover:bg-secondary border-primary bg-primary btn-primary' onClick={() => handleFormOpen('products', {
-                title: '',
-                tagline: '',
-                categories: [],
-                description: '',
-                price: 0,
-                content: '',
-                expiryDate: '',
-                allergens: [],
-                delivery: '',
-                storage: '',
-                ingredients: '',
-                nutrition: '',
-                countInStock: 0,
-                imageUrl: '',
-              })}
-              >新增商品</button>
+              <button className='block mt-3 ml-auto text-sm w-fit text-secondary hover:text-primary hover:bg-secondary border-primary bg-primary btn-primary' onClick={() => {
+                setProductForm({
+                  _id: '',
+                  title: '',
+                  tagline: '',
+                  categories: [],
+                  description: '',
+                  price: 0,
+                  content: '',
+                  expiryDate: '',
+                  allergens: [],
+                  delivery: '',
+                  storage: '',
+                  ingredients: '',
+                  nutrition: '',
+                  countInStock: 0,
+                  imageUrl: '',
+                  updatedAt: '',
+                })
+                setIsAddOrEditProductForm('add')
+              }}>新增商品</button>
             </section>
             <section className='flex-1'>
               <h3 className="mb-2 text-base font-medium">最新消息管理</h3>
@@ -232,7 +239,10 @@ const Dashboard = () => {
                   <li className='flex items-center justify-between py-3 text-xs border-b border-gray-300 border-dashed cursor-pointer' key={post._id}>
                     <p className='hover:opacity-80'>{post.title}</p>
                     <div className='space-x-4'>
-                      <button className='hover:opacity-60' onClick={() => handleFormOpen('posts', post)}>
+                      <button className='hover:opacity-60' onClick={() => {
+                        setPostForm(post)
+                        setIsAddOrEditPostForm('edit')
+                      }}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 50 50">
                           <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z"></path>
                         </svg>
@@ -247,13 +257,18 @@ const Dashboard = () => {
                   </li>
                 ))}
               </ul>
-              <button className='block mt-3 ml-auto text-sm w-fit text-secondary hover:text-primary hover:bg-secondary border-primary bg-primary btn-primary' onClick={() => handleFormOpen('posts', {
-                title: '',
-                category: '',
-                date: '',
-                content: '',
-                imageUrl: '',
-              })}>新增消息</button>
+              <button className='block mt-3 ml-auto text-sm w-fit text-secondary hover:text-primary hover:bg-secondary border-primary bg-primary btn-primary' onClick={() => {
+                setPostForm({
+                  _id: '',
+                  title: '',
+                  category: '',
+                  date: '',
+                  content: '',
+                  imageUrl: '',
+                  updatedAt: '',
+                })
+                setIsAddOrEditPostForm('add')
+              }}>新增消息</button>
             </section>
           </div>
           <div className='w-full'>
