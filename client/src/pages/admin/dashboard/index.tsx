11b1e35d022799fc55609d09739edbc41f83e4a8
kir4che <mollydcxxiii@gmail.com>
2024-01-23 02:43:43 +0800

    Update

--- client/src/pages/admin/dashboard/index.tsx
+++ client/src/pages/admin/dashboard/index.tsx
@@ -177,26 +177,6 @@ const Dashboard = () => {
     }
   }
 
-  const renderDialog = item => (
-    <li className='flex items-center justify-between py-3 text-xs border-b border-gray-300 border-dashed cursor-pointer' key={item._id}>
-      <p className='hover:opacity-80'>{item.title}</p>
-      <div className='space-x-4'>
-        <button className='hover:opacity-60' onClick={() => handleFormOpen(`${item}s`, item)}>
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 50 50">
-            <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z"></path>
-          </svg>
-        </button>
-        {item.toString() === 'product' ? <ProductEditForm isAddOrEdit={isAddOrEditProductForm} setIsAddOrEdit={setIsAddOrEditProductForm} form={productForm} setForm={setProductForm} handleFormUpdate={handleFormUpdate} />
-          : <PostEditForm isAddOrEdit={isAddOrEditPostForm} setIsAddOrEdit={setIsAddOrEditPostForm} form={postForm} setForm={setPostForm} handleFormUpdate={handleFormUpdate} />}
-        <button className='hover:opacity-60' onClick={() => handleDelete(`${item}s`, item._id)}>
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 24 24">
-            <path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
-          </svg>
-        </button>
-      </div>
-    </li>
-  )
-
   return (
     <Layout>
       {role.includes('admin') &&
@@ -208,7 +188,24 @@ const Dashboard = () => {
             <section className='flex-1'>
               <h3 className="mb-2 text-base font-medium">商品管理</h3>
               <ul className='p-3 overflow-y-auto border shadow max-h-80'>
-                {products.map(product => renderDialog(product))}
+                {products.map(product => (
+                  <li className='flex items-center justify-between py-3 text-xs border-b border-gray-300 border-dashed cursor-pointer' key={product._id}>
+                    <p className='hover:opacity-80'>{product.title}</p>
+                    <div className='space-x-4'>
+                      <button className='hover:opacity-60' onClick={() => handleFormOpen('products', product)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 50 50">
+                          <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z"></path>
+                        </svg>
+                      </button>
+                      <ProductEditForm isAddOrEdit={isAddOrEditProductForm} setIsAddOrEdit={setIsAddOrEditProductForm} form={productForm} setForm={setProductForm} handleFormUpdate={handleFormUpdate} />
+                      <button className='hover:opacity-60' onClick={() => handleDelete('products', product._id)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 24 24">
+                          <path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </li>
+                ))}
               </ul>
               <button className='block mt-3 ml-auto text-sm w-fit text-secondary hover:text-primary hover:bg-secondary border-primary bg-primary btn-primary' onClick={() => handleFormOpen('products', {
                 title: '',
@@ -231,7 +228,24 @@ const Dashboard = () => {
             <section className='flex-1'>
               <h3 className="mb-2 text-base font-medium">最新消息管理</h3>
               <ul className='p-4 overflow-y-auto border shadow max-h-80'>
-                {posts.map(post => renderDialog(post))}
+                {posts.map(post => (
+                  <li className='flex items-center justify-between py-3 text-xs border-b border-gray-300 border-dashed cursor-pointer' key={post._id}>
+                    <p className='hover:opacity-80'>{post.title}</p>
+                    <div className='space-x-4'>
+                      <button className='hover:opacity-60' onClick={() => handleFormOpen('posts', post)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 50 50">
+                          <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z"></path>
+                        </svg>
+                      </button>
+                      <PostEditForm isAddOrEdit={isAddOrEditPostForm} setIsAddOrEdit={setIsAddOrEditPostForm} form={postForm} setForm={setPostForm} handleFormUpdate={handleFormUpdate} />
+                      <button className='hover:opacity-60' onClick={() => handleDelete('posts', post._id)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 24 24">
+                          <path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </li>
+                ))}
               </ul>
               <button className='block mt-3 ml-auto text-sm w-fit text-secondary hover:text-primary hover:bg-secondary border-primary bg-primary btn-primary' onClick={() => handleFormOpen('posts', {
                 title: '',
