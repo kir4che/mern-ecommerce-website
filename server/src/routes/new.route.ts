f7d8a00e7340bd9fad3452b2635d89961f99dda0
kir4che <mollydcxxiii@gmail.com>
2024-11-27 22:02:37 +0800

    feat: implement news routes and controller, replace post functionality

---
+++ server/src/routes/new.route.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  addNew,
+  deleteNewById,
+  getNew,
+  getNewById,
+  updateNew,
+} from "@/controllers/news.controller";
+const router = Router();
+
+router.route("/").get(getNew);
+router.route("/:id").get(getNewById);
+router.route("/").post(addNew);
+router.route("/:id").patch(updateNew);
+router.route("/:id").delete(deleteNewById);
+
+export { router as newRouter };
