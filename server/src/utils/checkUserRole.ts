08053b7c1849b2a170fff5915516225bd6e0fdc7
kir4che <mollydcxxiii@gmail.com>
2025-02-05 14:22:32 +0800

    fix: 修復使用者參數可能 undefined 的情況

--- server/src/utils/checkUserRole.ts
+++ server/src/utils/checkUserRole.ts
@@ -1,17 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 
 // 檢查使用者權限
-const checkUserRole = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+const checkUserRole = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session.user) return res.status(401).json({ message: "Unauthorized. Please log in." });
+
   let { role } = req.session.user;
   if (role == "admin") next();
-  else
-    res
-      .status(403)
-      .json({ message: "Permission denied. Only admins can add products." });
+  else res.status(403).json({ message: "Permission denied. Only admins can add products." });
 };
 
 export default checkUserRole;
