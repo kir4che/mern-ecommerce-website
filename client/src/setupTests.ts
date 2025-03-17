11469f7bd040e878004d799eac66ae63ed645096
kir4che <mollydcxxiii@gmail.com>
2025-03-17 23:53:49 +0800

    chore: 更新 Jest 配置，並升級相關依賴版本。

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,10 +1,6 @@
 import "@testing-library/jest-dom";
 
-// Add TextEncoder polyfill
-if (typeof TextEncoder === "undefined") {
+if (typeof TextEncoder === "undefined")
   global.TextEncoder = require("util").TextEncoder;
-}
-
-if (typeof TextDecoder === "undefined") {
+if (typeof TextDecoder === "undefined")
   global.TextDecoder = require("util").TextDecoder;
-}
